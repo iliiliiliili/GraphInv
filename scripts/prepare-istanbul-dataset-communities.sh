7893e5434753fef667e881529b6a1e32396ff3bc
Illia Oleksiienko <io@ece.au.dk>
2024-04-17 20:33:29 +0200

    Update paths

--- scripts/prepare-istanbul-dataset-communities.sh
+++ scripts/prepare-istanbul-dataset-communities.sh
@@ -1,2 +1,2 @@
 cd ../src
-node -r ts-node/register --max-old-space-size=600000 --max-semi-space-size=600000 /mnt/d/FILES/Code/DeepFINA/GraphInv/src/prepare-istanbul-dataset.ts createCommunities
+node -r ts-node/register --max-old-space-size=600000 --max-semi-space-size=600000 prepare-istanbul-dataset.ts createCommunities
